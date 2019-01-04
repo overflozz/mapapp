4ab4c6b52f6a105b7d35060ccd3fdc5a0f677efe
overflozz <valentin.chelle@hotmail.fr>
2019-01-04 14:48:33 +0100

    intial commit

---
+++ src/client/Map/Map.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+
+class Map extends Component {
+  render() {
+    return <p>Loading profile...</p>;
+  }
+}
+
+export default withRouter(Map);
